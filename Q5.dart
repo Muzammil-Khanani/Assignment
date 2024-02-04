45d47f4b5e631cf5ee7b5897947386c2df788e55
Muzammil-Khanani <muzammilkhanani32@gmail.com>
2024-02-04 17:17:30 +0500

    Assignment 2 Q5

---
+++ Q5.dart
@@ -0,0 +1,10 @@
+void main() {
+  print(
+      "Q.5: Create a map with name, phone keys and store some values to it. Use where to find all keys that have length 4");
+  List<Map> phone = {
+    "muzammil": 1515,
+    "ali": 85648,
+    "Hamza": 51425,
+    "Ahmed": 1475
+  };
+}
